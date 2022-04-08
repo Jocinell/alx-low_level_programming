4ff9418c3d0906428096fa2095ea84511391dd44
jocinell <afolagbadejoy8@gmail.com>
2022-04-07 18:46:58 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-	fprintf(
-		stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	retun (1);
+	        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);         
+		        retun (1);
 }
