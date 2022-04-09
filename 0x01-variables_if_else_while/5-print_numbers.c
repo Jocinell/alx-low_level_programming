5d32112978588534a664346483a0ec70c5d1a5ac
jocinell <afolagbadejoy8@gmail.com>
2022-04-09 05:54:34 -0700

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,41 +3,18 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	int j;
-	int k;
-	int w;
 
-	for (i = 0; i < 10; i++)
+	i = 0;
+	while (i <10)
 	{
-		for (j = 0; j < 9; j++)
-		{
-			for (k = 0; k < 10; k++)
-			{
-				for (w = 1; w < 10; w++)
-				{
-					if (i != k && j != w)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(32);
-						putchar(k + '0');
-						putchar(w + '0');
-						if (i == 9 && j == 8 && k == 9 && w == 9)
-							putchar(10);
-						else
-						{
-							putchar(44);
-							putchar(32);
-						}
-					}
-				}
-			}
-		}
+		printf("%d", i);
+		i++;
 	}
+	putchar(10);
 	return (0);
 }
