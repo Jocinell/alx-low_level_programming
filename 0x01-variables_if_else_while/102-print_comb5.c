a6f87d8ad4f4b66d8f7ccaf54b1b3212ee145559
jocinell <afolagbadejoy8@gmail.com>
2022-04-09 12:51:55 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints 3 combination of numbers
  *
- * Return: Always 0 (Sucess)
+ * Return Always (Sucess)
  */
 int main(void)
 {
-	int i;
-	int j;
-	int k;
-	int w;
-
-	for (i = 0; i < 10; i++)
+	int num1, num2;
+	
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (j = 0; j < 9; j++)
-		{
-			for (k = 0; k < 10; k++)
-			{
-				for (w = 1; w < 10; w++)
-				{
-					if (i != k && j != w)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(32);
-						putchar(k + '0');
-						putchar(w + '0');
-						if (i == 9 && j == 8 && k == 9 && w == 9)
-							putchar(10);
-						else
-						{
-							putchar(44);
-							putchar(32);
-						}
-					}
-				}
-			}
-		}
+	for (num2 = num1 + 1; num2 <= 99; num2++)
+	{
+	putchar((num1 / 10) + '0');
+	putchar((num1 % 10) + '0');
+	puchar(' ');
+	putchar((num2 / 10) = '0');
+	putchar((num2 % 10) + '0');
+
+	if (num1 == 98 && num2 == 99)
+	continue;
+
+	putchar(',');
+	putchar (' ');
 	}
+	}
+	putchar('\n');
+
 	return (0);
 }
