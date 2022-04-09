2a6c7cbcba05f9d227a0862d37cb8a4bacd58d6a
jocinell <afolagbadejoy8@gmail.com>
2022-04-09 09:33:05 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for(j = 1; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
-			if(i < j)
+			if (i < j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
