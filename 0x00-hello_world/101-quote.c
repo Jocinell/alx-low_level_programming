5c337b66dccac3ad1f0360c72df45c6361ba6cd5
jocinell <afolagbadejoy8@gmail.com>
2022-04-10 10:51:29 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
 	fprintf(
-		stderr, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
+		stderr, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 	       );
 	return (1);
 }
