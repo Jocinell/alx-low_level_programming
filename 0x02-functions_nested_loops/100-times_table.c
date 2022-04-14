d0ca5eab5f2002a8c9a06a0b950ea9ebde133d2e
jocinell <afolagbadejoy8@gmail.com>
2022-04-14 04:50:22 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,44 @@
 #include "main.h"
-
 /**
- * print_times_table - print times table
- * @n : times table to use
- * Return:void
+ * print_times_table - output times table
+ * @n : specified number
  */
-
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
-	
-	if (n < 0 || n > 15)
-		return;
-	while (a <= n)
+	int product, bigger, i, j;
+
+	if((n >= 0) && (n <= 15))
 	{
-		for (b = 0; b <= n; b++)
+		for (i = 0; i <= n; i++)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
+			_putchar(48);
+			for (j = 1; j <= n; j++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar(('0' + rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
+				product = i * j;
 				_putchar(',');
 				_putchar(' ');
+
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 100)
+				{
+					bigger = product / 10;
+					_putchar(bigger / 10 + '0');
+					_putchar(bigger % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else
+				{
+					_putchar (' ');
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		a++;
 	}
 }
