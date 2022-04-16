bb4fc325792853906d4e29ba18e95bf90468dec7
jocinell <afolagbadejoy8@gmail.com>
2022-04-16 06:54:26 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - test positive_or_negative(i)
+ * main - test postive_or_negative(i)
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int i;
 
-	int = 0;
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
