26a520b424585d8d38f3e7be0a9ef1c2c62220b0
jocinell <afolagbadejoy8@gmail.com>
2022-04-20 11:46:29 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,16 @@
-#include "main.h"
 /**
- * _puts - function that prints a string,
- * followed by a new line, to stdout.
- * @str: sting to print
+ * _strlen - function that returns the length of a string.
+ * @s: string to check
  *
- * Return: void
+ * Return: Integer
  */
-void _puts(char *str)
+
+int _strlen(char *s)
 {
 	int counter;
 
 	counter = 0;
-	while (*(str + counter) != '\0')
-	{
-		_putchar(str[counter]);
+	while (*(s + counter) != '\0')
 		counter++;
-	}
-	_putchar('\n');
+	return (counter);
 }
