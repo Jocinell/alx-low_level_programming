f778a8118cbc2201cd6ac577171d27d891bf899a
jocinell <afolagbadejoy8@gmail.com>
2022-05-02 06:46:07 -0700

    task0

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+void main(int n)
 {
 	int n;
 	int a[5];
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,4 @@ char *leet(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *, int size_r);
 void print_buffer(char *b, int size);
+void main(int n);
