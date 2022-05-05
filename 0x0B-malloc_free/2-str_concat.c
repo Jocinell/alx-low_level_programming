f75696692a50a829a8b410aa9a4d5bbf3e2cd447
jocinell <afolagbadejoy8@gmail.com>
2022-05-05 08:45:52 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,44 @@
-nes (34 sloc)  639 Bytes
-   
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
+ * str_concat - concatenates two strings
+ * @s1: string input one
+ * @s2: string input two
  *
- * Return: pointer of an array of chars
+ * Return: char
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *strout;
-	unsigned int i, j, k, limit;
+char *cat;
+char *nul = "";
+unsigned int i, j, x, y;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-
-	strout = malloc(sizeof(char) * (i + j + 1));
-
-	if (strout == NULL)
-	{
-		free(strout);
-		return (NULL);
-	}
-
-	for (k = 0; k < i; k++)
-		strout[k] = s1[k];
-
-	limit = j;
-	for (j = 0; j <= limit; k++, j++)
-		strout[k] = s2[j];
-
-	return (strout);
+i = j = x = y = 0;
+if (s1 == NULL)
+s1 = nul;
+if (s2 == NULL)
+s2 = nul;
+while (s1[i] != '\0')
+i += 1;
+while (s2[j] != '\0')
+j += 1;
+j += 1;
+cat = malloc((i + j) *sizeof(*cat));
+if (cat == NULL)
+return (NULL);
+while (s1[x] != '\0')
+{
+cat[x] = s1[x];
+x += 1;
+}
+while (s2[y] != '\0')
+{
+cat[x] = s2[y];
+y += 1;
+x += 1;
+}
+cat[x] = '\0';
+return (cat);
 }
